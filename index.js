5a6d6ae8b967a9357127c768a0817260df9c7bcf
Mahamudurkhan10 <mahamudurkhan10@gmail.com>
2024-05-13 14:38:39 +0600

    mongo db user connect

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run() {
      try {
           // Connect the client to the server	(optional starting in v4.7)
           //     await client.connect();
+          const foodCollection = client.db('FoodShare').collection('allfoods');
+          app.get('/foods',async(req,res)=>{
+               const cursor = foodCollection.find();
+               const result = await cursor.toArray();
+               res.send(result)
+          })
 
           // Send a ping to confirm a successful connection
           await client.db("admin").command({ ping: 1 });
